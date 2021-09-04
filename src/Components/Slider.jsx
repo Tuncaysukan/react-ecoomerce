0775dc00b73a5f4295138ab179770543c68c93f7
tuncaysukan <tuncay_sukan@hotmail.com>
2021-09-04 11:31:40 +0300

    slider
    slider

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import ArrowRightOutlinedIcon from '@material-ui/icons/ArrowRightOutlined';
 import ArrowLeftOutlinedIcon from '@material-ui/icons/ArrowLeftOutlined';
@@ -7,8 +7,8 @@ const Containter = styled.div`
 height: 100vh;
 width: 100%;
 display: flex;
-background-color: coral;
 position: relative;
+overflow: hidden;
 
 `;
 const Arrow = styled.div`
@@ -19,6 +19,7 @@ border-radius: 50%;
 display: flex;
 align-items: center;
 position: absolute;
+justify-content: center;
 top: 0;
 bottom: 0;
 left: ${props => props.direction === 'left' && '10px'};
@@ -26,41 +27,95 @@ right: ${props => props.direction === 'right' && '10px'};
 margin: auto;
 cursor: pointer;
 opacity: 0.50;
+z-index: 2;
 `;
 
 const Wrapper = styled.div`
 height: 100%;
+display: flex;
+transform: translateX(0vw);
 `;
 const Slide = styled.div`
 width: 100vw;
 height: 100vh;
 display: flex;
 align-items: center;
+background-color: #${props=>props.bg};
 
 `;
 const ImgContainer = styled.div`
+height: 100%;
 flex:1;`;
 const Image = styled.img`
+
 height:80%`;
 const InfoContainer = styled.div`
-flex:1;`;
+flex:1;
+padding: 50px;
+`;
+const Title = styled.h1`
+font-size: 70px;
+`;
+const Desc = styled.p`
+margin: 50px 0px;
+font-size: 20px;
+font-weight: 500;
+letter-spacing: 3px;
+`;
+const Button = styled.button`
+padding:10px;
+font-size: 20px;
+background-color: transparent;
+cursor: pointer;
+border-radius: 20%;
+`;
 
 export const Slider = () => {
+    const [slideIndex,setSlidex]=useState(0)
+    const  handelClick=(direction)=>{
+        
+    };
     return (
         <Containter>
-            <Arrow direction='left'>
+            <Arrow direction='left' onClick={()=>handelClick('left')}>
                 <ArrowLeftOutlinedIcon />
             </Arrow>
             <Wrapper>
-                <Slide>
-                <ImgContainer>
-                    <Image src='https://i.ibb.co/XsdmR2c/1.png' />
-                </ImgContainer>
-                <InfoContainer></InfoContainer>
+           
+                <Slide bg='f5fafd'>
+                    <ImgContainer>
+                        <Image src='https://i.ibb.co/XsdmR2c/1.png' />
+                    </ImgContainer>
+                    <InfoContainer>
+                        <Title>TBT Kampanyası</Title>
+                        <Desc>Lorem ipsum dolor sit amet,</Desc>
+                        <Button>Mağza</Button>
+                    </InfoContainer>
                 </Slide>
-            </Wrapper>
+                <Slide bg='f5f1ed'>
+                    <ImgContainer>
+                        <Image src='https://i.ibb.co/XsdmR2c/1.png' />
+                    </ImgContainer>
+                    <InfoContainer>
+                        <Title>Yaz Kampanyası</Title>
+                        <Desc>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos repellendus accusamus quibusdam corrupti alias nemo vel laborum nihil ipsa dolores necessitatibus, perferendis hic amet magnam, sapiente labore debitis animi totam.</Desc>
+                        <Button>Mağza</Button>
+                    </InfoContainer>
+                </Slide>
+                <Slide bg='f5f0f4'>
+                    <ImgContainer>
+                        <Image src='https://i.ibb.co/XsdmR2c/1.png' />
+                    </ImgContainer>
+                    <InfoContainer>
+                        <Title>TBT-2 Kampanyası</Title>
+                        <Desc>Lorem ipsum dolor sit amet,</Desc>
+                        <Button>Mağza</Button>
+                    </InfoContainer>
+                </Slide>
+            
+               </Wrapper>
 
-            <Arrow direction='right'>
+            <Arrow direction='right' onClick={()=>handelClick('right')}>
                 <ArrowRightOutlinedIcon />
             </Arrow>
 
